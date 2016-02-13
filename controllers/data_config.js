4b2550b3dbd3ea054132a1709da22b53f6577a9f
jerson@hotmail.com <jerson@hotmail.com>
2016-02-12 23:07:48 -0500

    actualizacion de bd

--- controllers/data_config.js
+++ controllers/data_config.js
@@ -1,9 +1,9 @@
 // Por motivos de seguridad este archivo debería estar añadido al .gitignore
 // Lo dejo sin añadir para que se vea la estructura
 exports.cloudinary = {
-	cloud_name: "codigoprueba",
-	api_key: "492842146257925",
-	api_secret: "dRFxEtaY-MZWNO3AqVk4JVhfwoI"
+	cloud_name: "djempmk2c",
+	api_key: "429628637822674",
+	api_secret: "jdxpup4Mm4jRigo87M3pMN6Rgzo"
 };
 
 exports.admin = {
